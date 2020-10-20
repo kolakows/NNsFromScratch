bbca528c85c63cd0fc2283c1465d33ed500bd910
Igor Kołakowski <kolakowski.igor@gmail.com>
2020-10-20 11:50:02 +0200

    fix cumulative grad array creation

--- network.py
+++ network.py
@@ -49,8 +49,8 @@ class Network():
             wgradcum, bgradcum = self._empty_grad()
             for x,y in data:
                 wgrad, bgrad = self.backprop(x,y)
-                wgradcum += wgrad # divide by len(data) now? (overflows?)
-                bgradcum += bgrad
+                wgradcum = [old_w + w for old_w, w in zip(wgradcum, wgrad)] # divide by len(data) now? (overflows?)
+                bgradcum = [old_b + b for old_b, b in zip(bgradcum, bgrad)]
 
             # descent part
             self.weights = [w - lr * wgrad / len(data) for w, wgrad in zip(self.weights, wgradcum)]
@@ -106,7 +106,7 @@ class Network():
         return self.forward(a)
 
     def _empty_grad(self):
-        wgrad = np.array([np.zeros(w.shape) for w in self.weights])
-        bgrad = np.array([np.zeros(b.shape) for b in self.biases])
+        wgrad = [np.zeros(w.shape) for w in self.weights]
+        bgrad = [np.zeros(b.shape) for b in self.biases]
         return wgrad, bgrad
 
