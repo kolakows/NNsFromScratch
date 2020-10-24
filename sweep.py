8ca072d89a07f8336ff69b2dc0b7e0cc9e25e954
Igor Kołakowski <kolakowski.igor@gmail.com>
2020-10-24 20:37:34 +0200

    correct return arguments in sweep

--- sweep.py
+++ sweep.py
@@ -24,13 +24,13 @@ def train():
     local_path = 'C:/MiniProjects/sem2/NeuralNets/'
 
     config_defaults = dict(
-        data = 'classification/data.three_gauss.test.100.csv',
-        layers = '[2,3]',
+        data = 'regression/data.activation.train.100.csv',
+        layers = '[1,4,1]',
         lr = 1,
         epochs = 30,
         seed = 123,
         loss_function = 'MSE',
-        activation_function = 'sigmoid',
+        activation_function = 'relu',
     )
 
     wandb.init(config=config_defaults)
@@ -50,9 +50,9 @@ def train():
     data = pd.read_csv(data_path, sep=',', header=0)
  
     if task_type == 'cls':
-        train, test, label_encoder = train_test_from_df_categorical(data, 'cls', 0.9, config.seed)
+        train, test, label_encoder, feature_scalers = train_test_from_df_categorical(data, 'cls', 0.9, config.seed)
     elif task_type == 'reg':
-        train, test = train_test_from_df_regression(data, 'y', 0.9, config.seed)
+        train, test, feature_scalers = train_test_from_df_regression(data, 'y', 0.9, config.seed)
 
 
     net = Network.from_config(config)
@@ -64,5 +64,3 @@ def train():
     wandb.log({'Test data plot': wandb.Image(get_data_plot(test, net))}, step=config.epochs)
     plt.close()
     #print(f'Category accuracy score: {net.evaluate_categorical(test)}')
-
-
