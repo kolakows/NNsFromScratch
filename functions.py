fix loss calculation

functions.py
@@ -74,8 +74,10 @@ class SE(loss_function):
         return output_activation - y
 
 class cross_entropy(loss_function):
+    def loss(self, output_activation, y):
+        return - (np.multiply(y, np.log(output_activation)) + np.multiply(np.subtract(1,y), np.log(np.subtract(1,output_activation))))
     def deriv(self, output_activation, y):
-        return - np.divide(y, output_activation) + np.divide(1 - y, 1 - output_activation)
+        return - np.divide(y, output_activation) + np.divide(np.subtract(1,y), np.subtract(1,output_activation))
 
 class MAE(loss_function):
     def loss(self, output_activation, y):

network.py
@@ -131,11 +131,7 @@ class Network():
             return math.sqrt(np.sum((y_n[0] - y_real[0])**2 for (y_n,y_real) in results) / len(results)), results
 
     def calculate_loss(self, data):
-        results = []
-        if self.task == 'cls':
-            results = [(np.argmax(self.forward(x)), np.argmax(y)) for (x,y) in data]
-        else:
-            results = [(self.forward(x), y) for (x,y) in data]
+        results = [(self.forward(x), y) for (x,y) in data]
         return np.sum([self.lossfun.loss(output, y) for output, y in results])
       
     def __call__(self, a):