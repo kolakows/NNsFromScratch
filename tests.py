28e23e276c28d7e9e999bbe7a07735ef435bd41f
Igor Kołakowski <kolakowski.igor@gmail.com>
2020-10-27 21:46:05 +0100

    tidy test.py

--- tests.py
+++ tests.py
@@ -10,24 +10,21 @@ import numpy as np
 # for regression select the size of the output layer as 1
 
 # random number generator seed (None for random seed)
-seed = None
+seed = 4
 
 # set 'reg' for regression and 'cls' for classification
-# task_type='cls'
-task_type = 'reg'
+task_type='cls'
+#task_type = 'reg'
 
 # number of perceptrons in each layer
 # classification: input - space dim, output - number of classes
 # regression: input - space dim, output - 1
-#net_arch = [2, 7, 7, 4]
-net_arch = [1, 7, 7, 1]
+net_arch = [2, 8, 8, 4, 4]
+#net_arch = [1, 64, 32, 16, 1]
 
 # path to train data
-#train_data_path = '/home/monika/Pulpit/SN/SN_projekt1/classification/data.three_gauss.test.100.csv'
-#train_data_path = '/home/monika/Pulpit/SN/SN_projekt1/regression/data.activation.train.1000.csv'
-#train_data_path = r'C:\MiniProjects\sem2\NeuralNets\classification\data.three_gauss.test.100.csv'
-train_data_path = r'C:\MiniProjects\sem2\NeuralNets\SN_projekt1_test\regression\data.multimodal.train.1000.csv'
-test_data_path = r'C:\MiniProjects\sem2\NeuralNets\SN_projekt1_test\regression\data.multimodal.test.1000.csv'
+train_data_path = r'C:\MiniProjects\sem2\NeuralNets\SN_projekt1_test\Classification\data.circles.train.1000.csv'
+test_data_path = r'C:\MiniProjects\sem2\NeuralNets\SN_projekt1_test\Classification\data.circles.test.1000.csv'
 
 #path to test data (to be used in the future)
 #currently test data is split
@@ -50,7 +47,7 @@ elif task_type == 'reg':
     train, test, _ = train_test_from_files_regression(train_data, test_data, 'y', seed)
     #train, test, feature_scalers = train_test_from_df_regression(data, 'y', 0.9, seed)
 
-net.GD(train, lr = 1, epochs = 100)
+net.GD(train, lr = 1, epochs = 1500, log_accuracy=False, plot_loss=True)
 
 test.sort(key = lambda val: val[0][0])
 score, results = net.evaluate(test)
